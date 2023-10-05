e5504a35ff914fd20c50a508c8e15a0df0e36889
thisislaait <ogadi.ugo@gmail.com>
2023-10-05 12:37:34 +0200

    New pages added

---
+++ africhow_version1/src/Pages/Campgrounds.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import Nav from "../Components/Nav";
+import Banner from "../Components/Banner";
+import campgroundData from "../Data/campgroundData";
+import CampgroundCard from "../Data/Cards";
+
+const Campgrounds = () => {
+  return (
+    <main className="row">
+      <Nav />
+      <section>
+        <Banner />
+      </section>
+      <section className="row card-content mx-12 md:mx-20 flex items-center flex-wrap md:flex gap-2.5 mb-10">
+        {campgroundData.map((campground) => (
+            <CampgroundCard key={campground.id} campground={campground} />
+        ))}
+      </section>
+    </main>
+  );
+};
+
+export default Campgrounds;
